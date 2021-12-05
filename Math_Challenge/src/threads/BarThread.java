04593dbdfba83ac7732aa81ef6f1889a2c69690b
Santiago Arboleda Velasco <santiagoarboledavelasco@gmail.com>
2021-12-05 15:16:25 -0500

    Se completa la clase BarThread

--- Math_Challenge/src/threads/BarThread.java
+++ Math_Challenge/src/threads/BarThread.java
@@ -1,5 +1,50 @@
 package threads;
 
-public class BarThread {
+import javafx.application.Platform;
+import ui.GUIController;
+
+
+public class BarThread extends Thread {
+	
+	private GUIController app;
+	private int time;
+	public BarThread(GUIController a) {
+		app = a;
+		time = 100;
+	}
+	
+	public void run(){
+		
+		
+		
+		while (time > 0) {
+			Platform.runLater(new Runnable() {
+				
+				@Override
+				public void run() {
+					
+					try {
+						
+						app.updateBar(time);
+						
+					} catch (Exception e) {
+						
+						
+					}
+					
+				}
+			});
+			
+			try {
+				
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
+			time --;
+			
+		}
+	}
 
 }
