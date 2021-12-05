fcfcefba06a03483a3593c0cfd9b856797e7b491
Santiago Arboleda Velasco <santiagoarboledavelasco@gmail.com>
2021-12-05 14:45:00 -0500

    Se agrega la clase Timer

--- Math_Challenge/src/threads/Timer.java
+++ Math_Challenge/src/threads/Timer.java
@@ -1,5 +1,58 @@
 package threads;
 
-public class Timer {
+import javafx.application.Platform;
+import ui.GUIController;
 
+
+public class Timer extends Thread {
+	
+	private int time;
+	private GUIController app;
+	
+	
+	public Timer(int time,GUIController a) {
+		this.time = time;
+		app = a;
+		
+	}
+		
+	
+	public void run() {
+		while (time > 0) {
+			Platform.runLater(new Runnable() {
+				
+				@Override
+				public void run() {
+					try {
+						app.updateSeconds();
+					}catch (Error e) {
+						
+					}
+					
+				}
+			});
+			
+			time --;
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		Platform.runLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					app.closeGame();
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+				
+			}
+		});
+	}
 }
